d94089d35fbfbca262acccff37d41db35dd4b082
zoey-yjj <zoeyyyjj@gmail.com>
2023-03-17 21:14:39 +0800

    setup analysis

---
+++ taintAnalysis.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <map>
+#include <stack>
+
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Support/SourceMgr.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Constants.h"
+
+using namespace llvm; // mainly use construct from llvm
+
+
+int main(int argc, char **argv) {}
