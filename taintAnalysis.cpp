072b8bc4740247c3f3b7a245a34b1f62fc6be07d
zoey-yjj <zoeyyyjj@gmail.com>
2023-03-18 10:07:34 +0800

    read IR file

--- taintAnalysis.cpp
+++ taintAnalysis.cpp
@@ -19,7 +19,20 @@ using namespace llvm; // mainly use construct from llvm
 std::set<std::string> intersect_sets(std::set<std::string> A, std::set<std::string> B);
 std::set<std::string> union_sets(std::set<std::string> A, std::set<std::string> B);
 
-int main(int argc, char **argv) {}
+
+int main(int argc, char **argv) {
+    // Read the IR file.
+    LLVMContext &Context = getGlobalContext(); 
+    SMDiagnostic Err;
+
+    // Extract Module M from IR (assuming only one Module exists)
+    Module *M = ParseIRFile(argv[1], Err, Context);
+    if (M == nullptr) {
+        fprintf(stderr, "error: failed to load LLVM IR file \"%s\"", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+}
 
 
 std::set<std::string> intersect_sets(std::set<std::string> A, std::set<std::string> B) {
