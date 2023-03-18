9c217ef975b8f5561fabaacef7bef4e3cfc3d4f5
zoey-yjj <zoeyyyjj@gmail.com>
2023-03-18 09:32:51 +0800

    union of sets

--- taintAnalysis.cpp
+++ taintAnalysis.cpp
@@ -17,6 +17,7 @@ using namespace llvm; // mainly use construct from llvm
 
 
 std::set<std::string> intersect_sets(std::set<std::string> A, std::set<std::string> B);
+std::set<std::string> union_sets(std::set<std::string> A, std::set<std::string> B);
 
 int main(int argc, char **argv) {}
 
@@ -25,4 +26,10 @@ std::set<std::string> intersect_sets(std::set<std::string> A, std::set<std::stri
 	std::set<std::string> result;
 	std::set_intersection(A.begin(), A.end(), B.begin(), B.end(), std::inserter(result, result.begin()));
     return result;
+}
+
+
+std::set<std::string> union_sets(std::set<std::string> A, std::set<std::string> B) {
+    A.insert(B.cbegin(), B.cend());
+    return A;
 }
